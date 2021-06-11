fac008dc921d479fc305c166d5d78dff6b02ae65
silencespeaker <b06902019@ntu.edu.tw>
2021-06-12 03:55:31 +0800

    Add a test.py for testing encryptiob class

--- test.py
+++ test.py
@@ -6,15 +6,15 @@ from Crypto.PublicKey import RSA
 import os, base64
 from Crypto import Random
 
-# random_generator = Random.new().read
-# rsa = RSA.generate(1024, random_generator)
-# with open("RSA_key/private_key.pem", "wb") as f:
-#     f.write(rsa.exportKey())
-# with open("RSA_key/public_key.pem", "wb") as f:
-#     f.write(rsa.public_key().exportKey())
-# rsa = RSAKey()
-# t = rsa.encode("Hello World !")
-# print(t)
-# print(rsa.decode(t))
+random_generator = Random.new().read
+rsa = RSA.generate(1024, random_generator)
+with open("RSA_key/private_key.pem", "wb") as f:
+    f.write(rsa.exportKey())
+with open("RSA_key/public_key.pem", "wb") as f:
+    f.write(rsa.public_key().exportKey())
+rsa = RSAKey()
+t = rsa.encode("Hello World !")
+print(t)
+print(rsa.decode(t))
 
 
