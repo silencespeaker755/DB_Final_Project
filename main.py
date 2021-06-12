457b429392676f3094b792d224c6fda5c1ce1165
silencespeaker <b06902019@ntu.edu.tw>
2021-06-12 11:33:28 +0800

    Add RSA method into User table

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import declarative_base
 from EncryptedString import EncrypedString
 from sqlalchemy.orm import sessionmaker
-from CryptoList import CaesarCipher
+from CryptoList import RSAKey
 from dotenv import load_dotenv
 import os
+import argparse
 
 load_dotenv()
 DB_URL = os.getenv('DB_URL')
@@ -15,12 +16,13 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(16))
-    fullname = Column(String(16))
-    nickname = Column(EncrypedString(CaesarCipher(3), 255))
+    name = Column(EncrypedString(RSAKey(), 255))
+    fullname = Column(EncrypedString(RSAKey(), 255))
+    nickname = Column(String(16))
+    key = Column((EncrypedString(RSAKey(), 255)))
 
     def __repr__(self):
-        return f"<User(name='{self.name}', fullname='{self.fullname}', nickname='{self.nickname}')>"
+        return f"<User(name='{self.name}', fullname='{self.fullname}', nickname='{self.nickname}', key='{self.key}')>"
 
 def create_db(db_name):
     engine = create_engine(DB_URL)
@@ -29,7 +31,12 @@ def create_db(db_name):
     conn.close()
 
 if __name__ == '__main__':
-    db_name = 'test_db'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--db_name", help="Choose current database")
+    parser.add_argument("-f", "--info", help="Load user's info & key")
+    args = parser.parse_args()
+
+    db_name = args.db_name
     try:
         create_db(db_name)
     except:
@@ -38,12 +45,16 @@ if __name__ == '__main__':
     engine = create_engine(f'{DB_URL}/{db_name}')
     Base.metadata.create_all(engine)
 
-    ed_user = User(name='ed', fullname='Ed Jones', nickname='eds')
+    with open(args.info) as f:
+        infos = [[t.strip() for t in line.split(" ")] for line in f.readlines()]
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.add(ed_user)
-    session.commit()
+    for info in infos:
+        user = User(name=info[0], fullname=info[1], nickname=info[2], key=info[3])
+        session.add(user)
+        session.commit()
 
-    our_user = session.query(User).filter_by(name='ed').all()
+    our_user = session.query(User).filter_by(nickname='dog').all()
     print(our_user)
